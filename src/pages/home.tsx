1edc5bef654d0f472a07fd85e54eba414bf29672
Christopher Vouga <crvouga@gmail.com>
2021-10-07 03:41:19 -0700

    added too much cheese

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,18 @@
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  AppBar,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import { AuthUserAvatar } from "../AuthUserAvatar";
 import LaunchIcon from "@mui/icons-material/Launch";
+import ReactPlayer from "react-player";
+
+const CHEESE_VIDEO_URL = "https://www.youtube.com/watch?v=3xPTR2lezGI";
 
 export const HomePage = () => {
   return (
@@ -19,6 +29,7 @@ export const HomePage = () => {
           </Toolbar>
         </Container>
       </AppBar>
+
       <Container
         maxWidth="sm"
         sx={{
@@ -33,6 +44,36 @@ export const HomePage = () => {
             Open GCU
           </Button>
         </Link>
+      </Container>
+      <Container disableGutters maxWidth="sm">
+        <Box
+          sx={{
+            overflow: "hidden",
+            position: "relative",
+            paddingTop: "56.25%",
+            height: 0,
+            width: "100%",
+            margin: "auto",
+          }}
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <ReactPlayer
+              controls
+              loop
+              width="100%"
+              height="100%"
+              url={CHEESE_VIDEO_URL}
+            />
+          </Box>
+        </Box>
       </Container>
     </>
   );
