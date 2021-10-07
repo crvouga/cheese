de5a6aaac26cc79f723a64f9acea7154a64b2d42
Christopher Vouga <crvouga@gmail.com>
2021-10-06 20:52:11 -0700

    made chacker happy

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,15 +1,8 @@
-import {
-  Button,
-  AppBar,
-  Box,
-  Divider,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import { BackButton } from "../BackButton";
-import { useProfile, useAuthUser, useAuth } from "../data-access";
+import { useAuth, useAuthUser, useProfile } from "../data-access";
 import { ProfileDisplayNameForm } from "./ProfileDisplayNameForm";
 import { ProfilePictureForm } from "./ProfilePictureForm";
 
