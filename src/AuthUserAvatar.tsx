fc9e302a48412c99712498f760c60cb1a6f9e21b
Christopher Vouga <crvouga@gmail.com>
2021-10-06 17:56:51 -0700

    fix auth user avatar

--- src/AuthUserAvatar.tsx
+++ src/AuthUserAvatar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Avatar } from "@mui/material";
+import { useAuthUser, useProfile } from "./data-access";
 
 export const AuthUserAvatar = () => {
+  const { userId } = useAuthUser();
+  const { profileState } = useProfile({ userId });
+
+  if (profileState.status === "success") {
+    return <Avatar src={profileState.profile.profilePictureUrl} />;
+  }
+
   return <Avatar />;
 };
