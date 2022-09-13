7357d94fdeba3df9b7410ee3134462c34e9862a5
Chris Vouga <crvouga@gmail.com>
2022-09-12 22:24:36 -0700

    better landing page

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,36 +1,43 @@
 import GoogleIcon from "@mui/icons-material/Google";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 import { useAuth } from "../data-access";
-import { PageLayout } from "../PageLayout";
 
 export const AuthPage = () => {
   const { signIn } = useAuth();
   return (
-    <>
-      <PageLayout
-        topBar={
-          <AppBar position="sticky">
-            <Toolbar>
-              <Typography variant="h6" sx={{ flex: 1 }} align="center">
-                Cheese 🧀
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        }
-        body={
-          <Container maxWidth="sm" sx={{ p: 2 }}>
-            <Button
-              startIcon={<GoogleIcon />}
-              onClick={() => {
-                signIn({ authMethod: "google" });
-              }}
-            >
-              Continue With Google
-            </Button>
-          </Container>
-        }
-      />
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        // transform: "translate(50%, 50%)",
+      }}
+    >
+      <Box sx={{ width: "100%", maxWidth: 360, padding: 2 }}>
+        <Typography variant="h1" align="center" sx={{ marginBottom: -1 }}>
+          🧀
+        </Typography>
+        <Typography variant="h2" align="center" sx={{ marginBottom: 3 }}>
+          cheese
+        </Typography>
+
+        <Button
+          fullWidth
+          startIcon={<GoogleIcon />}
+          onClick={() => {
+            signIn({ authMethod: "google" });
+          }}
+        >
+          Continue With Google
+        </Button>
+      </Box>
+    </Box>
   );
 };
